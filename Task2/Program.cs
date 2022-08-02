a69d14f91ffc69785d9d75632e22fffd047e2693
SilverXuz <silver-xuz@mail.ru>
2022-08-03 01:12:27 +0300

    Программа, которая принимает на вход число и выдаёт сумму цифр в числе.

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+while (n > 0)
+{
+    summa = summa + n % 10;
+    n /= 10;
+}
+Console.Write("Сумма: " + summa);
